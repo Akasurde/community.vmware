bfe529c14a6e48852120c43134707a3e4b2c05cd
Christian Kotte <christian.kotte@gmx.de>
2020-12-02 17:56:58 +0100

    Update plugins/modules/vmware_dvs_portgroup.py
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/modules/vmware_dvs_portgroup.py
+++ plugins/modules/vmware_dvs_portgroup.py
@@ -3,7 +3,7 @@
 
 # Copyright: (c) 2015, Joseph Callen <jcallen () csc.com>
 # Copyright: (c) 2017, Ansible Project
-# Copyright: (c) 2018,2020 Christian Kotte (@ckotte)
+# Copyright: (c) 2018, Christian Kotte (@ckotte)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
